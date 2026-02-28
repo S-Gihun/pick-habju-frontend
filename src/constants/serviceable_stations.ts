2d31201836fb452573e19cdbde50a5328107d0a9
S-Gihun <thsrlgns05@naver.com>
2026-02-28 20:15:05 +0000

    ✨ [#148]; feat: zoom 값 16으로 수정

--- src/constants/serviceable_stations.ts
+++ src/constants/serviceable_stations.ts
@@ -19,28 +19,28 @@ const SERVICEABLE_STATIONS: ServiceableStation[] = [
     name: '이수',
     subwayLine: '4호선·7호선',
     center: { lat: 37.485196, lng: 126.981605 },
-    bounds: { swLat: 37.4845, swLng: 126.9798, neLat: 37.4885, neLng: 126.9838 },
+    bounds: { swLat: 37.480196, swLng: 126.975605, neLat: 37.490196, neLng: 126.987605 },
   },
   {
     id: 'sadang',
     name: '사당',
     subwayLine: '2호선·4호선',
     center: { lat: 37.476550, lng: 126.981688 },
-    bounds: { swLat: 37.470000, swLng: 126.974000, neLat: 37.483000, neLng: 126.989000 },
+    bounds: { swLat: 37.471550, swLng: 126.975688, neLat: 37.481550, neLng: 126.987688 },
   },
   {
     id: 'hongdae',
     name: '홍대입구',
     subwayLine: '2호선·공항철도·경의중앙선',
     center: { lat: 37.556748, lng: 126.923643 },
-    bounds: { swLat: 37.554748, swLng: 126.921643, neLat: 37.558748, neLng: 126.925643 },
+    bounds: { swLat: 37.551748, swLng: 126.917643, neLat: 37.561748, neLng: 126.929643 },
   },
   {
     id: 'sinchon',
     name: '신촌',
     subwayLine: '2호선',
     center: { lat: 37.555153, lng: 126.936890 },
-    bounds: { swLat: 37.553153, swLng: 126.934890, neLat: 37.557153, neLng: 126.938890 },
+    bounds: { swLat: 37.550153, swLng: 126.930890, neLat: 37.560153, neLng: 126.942890 },
   },
 ];
 
